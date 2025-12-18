d014824342700adcb56415bf963088e2be81ad94
Gardas Akash <134189310+akashgardas@users.noreply.github.com>
2025-12-18 14:22:52 +0530

    Fix CSS file path in app_slr_tipsdataset.py

--- Linear_Regression_SLR/SLR_app_tipsdataset/app_slr_tipsdataset.py
+++ Linear_Regression_SLR/SLR_app_tipsdataset/app_slr_tipsdataset.py
@@ -18,7 +18,7 @@ def load_css(file):
     with open(file) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-load_css('./styles.css')
+load_css('styles.css')
 
 st.markdown('''
             <div class='card'>
