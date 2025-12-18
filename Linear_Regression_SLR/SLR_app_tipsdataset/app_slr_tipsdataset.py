3de425144cdeb7dfc309e4430bae029be52eb17f
Gardas Akash <134189310+akashgardas@users.noreply.github.com>
2025-12-18 14:21:36 +0530

    Import pathlib for file path management
    Added import for pathlib to handle file paths.

--- Linear_Regression_SLR/SLR_app_tipsdataset/app_slr_tipsdataset.py
+++ Linear_Regression_SLR/SLR_app_tipsdataset/app_slr_tipsdataset.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
